5b15a03645fddc36f1306175ede5d6707fa1315d
Khoi Nguyen <nvkhoi@intern.nvkhoi>
2018-06-04 15:42:54 +0700

    Them lay thong tin sinh vien theo id

--- src/main/java/com/example/demo3/Controller/StudentController.java
+++ src/main/java/com/example/demo3/Controller/StudentController.java
@@ -4,6 +4,8 @@ import com.example.demo3.Model.Student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -19,12 +21,14 @@ public class StudentController {
         return studentRespository.findAll();
     }
 //Lay thong tin sinh vien dua theo id
-    /*@GetMapping("/student/{id}")
-    public void getInfo(@PathVariable String id){
+    @GetMapping("/student/{id}")
+    public Student getInfo(@PathVariable String id){
         int studentID = Integer.parseInt(id);
-        Optional<Student> opt = studentRespository.findById(studentID);
-        System.out.print(opt.toString());
-    }*/
+        Student student1 = studentRespository.getOne(studentID);
+        Student student2 = new Student(student1);
+        System.out.print(student2);
+        return student2;
+    }
 //Dang lam
     /*
     @PostMapping("/student/search")
@@ -37,7 +41,14 @@ public class StudentController {
     public Student create(@RequestBody Map<String, String> body){
         String name = body.get("name");
         String studentClass = body.get("studentClass");
-        return studentRespository.save(new Student(name,studentClass));
+        Date dateOfBirth = new Date();
+        try {
+            dateOfBirth = new SimpleDateFormat("dd/MM/yyyy").parse(body.get("dateOfBirth"));
+        }
+        catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return studentRespository.save(new Student(name, studentClass, dateOfBirth));
     }
 //Sua 1 thong tin sinh vien
     @PutMapping("/student/{id}")
--- src/main/java/com/example/demo3/Model/Student.java
+++ src/main/java/com/example/demo3/Model/Student.java
@@ -4,21 +4,31 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.Date;
 
 @Entity
 public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
-
     private String name;
     private String studentClass;
+    private Date dateOfBirth;
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
 
     public Student() {  }
 
-    public Student(String name, String studentClass) {
+    public Student(String name, String studentClass, Date dateOfBirth) {
         this.name = name;
         this.studentClass = studentClass;
+        this.dateOfBirth = dateOfBirth;
     }
 
     public int getId() {
@@ -51,6 +61,14 @@ public class Student {
                 "id=" + id +
                 ", name='" + name + '\'' +
                 ", studentClass='" + studentClass + '\'' +
+                ", dateOfBirth=" + dateOfBirth +
                 '}';
+    }
+
+    public Student(Student student){
+        this.id = student.getId();
+        this.name = student.getName();
+        this.studentClass = student.getStudentClass();
+        this.dateOfBirth = student.getDateOfBirth();
     }
 }
